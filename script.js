4973a11d3ae65169f3530207b201b48183315ed5
Poonam <poonamsoni@Poonams-MacBook-Air.local>
2022-11-02 18:05:01 +0530

    buttons script updated

--- script.js
+++ script.js
@@ -7,21 +7,15 @@ function digitalClock() {                 //Shows Current Local Time
     min = min < 10 ? "0" + min : min;
     var timerNow = hours + ":" + min;
     timing.innerText = timerNow;
-    document.cont.appendChild(timing);
+    document.body.appendChild(timing);
 }
 
-window.onload = function() {
-    setInterval(digitalClock, 1000);
-  };
-
-var one = document.getElementById('button1');
-var two = document.getElementById('button2');
-var three = document.getElementById('button3');
-var plus = document.getElementById('buttonPlus');
-var scr = document.getElementById('screen');
-
-function one() {
-    var one = document.getElementById('button1');
-    one.innerHTML = 2;
-    console.log(one);
+let result = "";
+let buttons = document.getElementsByTagName('button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (event) {
+        result = result + event.target.innerHTML;
+        document.querySelector(screen).value = result;
+    }
+    )
 }
